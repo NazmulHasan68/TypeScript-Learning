0422323dc219ca32687dc975a71fb3c716c38d1b
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-11 21:33:42 +0600

    create controler and route

--- nodejs/src/controllers/todo.ts
+++ nodejs/src/controllers/todo.ts
@@ -1,5 +1,10 @@
 import { RequestHandler } from "express"
+import { Todo } from "../models/todo.model"
 
+const TODO: Todo[] = []
 export const createTodo :RequestHandler = (req, res)=>{
+    const text = (req.body as {text :string}).text
+    const text = (<{text:string}>req.body).text
 
+    
 }
